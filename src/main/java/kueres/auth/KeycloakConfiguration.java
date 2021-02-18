506a4f4fc9a220088c64bd8581a7a38b2d7bca39
Tim Engbrocks <uynhg@student.kit.edu>
2021-02-18 01:24:08 +0100

    Removed cors

--- src/main/java/kueres/auth/KeycloakConfiguration.java
+++ src/main/java/kueres/auth/KeycloakConfiguration.java
@@ -1,13 +1,10 @@
 package kueres.auth;
 
-import java.util.Arrays;
-
 import org.keycloak.adapters.KeycloakConfigResolver;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -18,28 +15,11 @@ import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(jsr250Enabled = true)
 public class KeycloakConfiguration extends KeycloakWebSecurityConfigurerAdapter {
-	
-	@Value("keycloak.auth-server-url")
-	private String keycloakUrl;
-	
-	@Bean
-	public CorsFilter corsFilter() {
-	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	    final CorsConfiguration config = new CorsConfiguration();
-	    config.setAllowCredentials(true);
-	    config.setAllowedOrigins(Arrays.asList("http://localhost:8080", this.keycloakUrl));
-	    config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-	    source.registerCorsConfiguration("/**", config);
-	    return new CorsFilter(source);
-	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -47,7 +27,6 @@ public class KeycloakConfiguration extends KeycloakWebSecurityConfigurerAdapter 
 		super.configure(http);
 		http.authorizeRequests().anyRequest().authenticated();
 		http.csrf().disable();
-		http.cors();
 		
 	}
 	
