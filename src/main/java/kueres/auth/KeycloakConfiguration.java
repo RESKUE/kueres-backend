083f731be1b182228ca2a473b825ab042834bd21
Tim Engbrocks <uynhg@student.kit.edu>
2021-03-13 16:13:24 +0100

    Only authenticate api routes

--- src/main/java/kueres/auth/KeycloakConfiguration.java
+++ src/main/java/kueres/auth/KeycloakConfiguration.java
@@ -10,11 +10,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+import kueres.base.BaseController;
 
 /**
  * 
@@ -32,15 +35,23 @@ import org.springframework.security.web.authentication.session.SessionAuthentica
 public class KeycloakConfiguration extends KeycloakWebSecurityConfigurerAdapter {
 	
 	/**
-	 * All requests must be authenticated.
+	 * All api requests must be authenticated.
 	 * Disables csrf protection.
 	 */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		super.configure(http);
-		http.authorizeRequests().anyRequest().authenticated();
+		http.authorizeRequests().antMatchers(BaseController.API_ENDPOINT + "/**").authenticated();
 		http.csrf().disable();
+		
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		
+		web.ignoring().antMatchers("/swagger-ui/**");
+		web.ignoring().antMatchers("/v3/**");
 		
 	}
 	
